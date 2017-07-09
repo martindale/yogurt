903b3268e5ab7df11efbe1656299bb99551b6abb
Eric Martindale <eric@ericmartindale.com>
2017-07-08 19:33:48 -0700

    Initial scratch-up of a demo

---
+++ assets/resources.js
@@ -0,0 +1,36 @@
+game.resources = [
+
+  /* Graphics.
+   * @example
+   * { name: 'example', type:'image', src: 'assets/data/img/example.png' },
+   */
+  { name: 'forest', type: 'image', src: 'assets/data/img/forest.png' },
+  { name: 'meta_collide', type: 'image', src: 'assets/data/img/meta_collide.png' },
+  { name: 'Blank_Sprite_Sheet_4_2_by_KnightYamato', type: 'image', src: 'assets/data/img/Blank_Sprite_Sheet_4_2_by_KnightYamato.png' },
+
+  { name: 'egg', type: 'image', src: 'assets/the-egg.png' },
+  { name: 'swampsnake', type: 'image', src: 'assets/swampsnake.png' },
+
+  /* Maps.
+   * @example
+   * { name: 'example01', type: 'tmx', src: 'assets/data/map/example01.tmx' },
+   * { name: 'example01', type: 'tmx', src: 'assets/data/map/example01.json' },
+    */
+  { name: 'isometric', type: 'tmx', src: 'assets/data/map/isometric.tmx' },
+
+  /* Background music.
+   * @example
+   * { name: 'example_bgm', type: 'audio', src: 'assets/data/bgm/' },
+   */
+
+  /* Sound effects.
+   * @example
+   * { name: 'example_sfx', type: 'audio', src: 'assets/data/sfx/' }
+   */
+
+  /* Atlases
+   * @example
+   * { name: 'example_tps', type: 'json', src: 'assets/data/img/example_tps.json' },
+   */
+
+];
